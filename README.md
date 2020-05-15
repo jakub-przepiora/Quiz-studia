7d29927905d07c6b2771b295b7a718b08fa1ca18
jakub-przepiora <jakub.przepioraa@gmail.com>
2020-05-16 00:45:23 +0200

    Read  question and create element on list

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # Quiz-studia
 # Zadanie zaliczenionwe Politechnika Śląska
-- Program działa 2pkt
-- Zaimplementowano pełną funkcjonalność 3pkt
-- Kod programu jest podzielony na moduły 2pkt
-- Długość funkcji nie przekracza jednego ekranu 1pkt
-- Zadania są dobrze podzielone między funkcję 2pkt
-- Kod jest samokomentujący się lub występują komentarze 1pkt
-- Kod jest poprawnie sformatowany (wcięcia, jedna instrukcja w jednej linii, itp.)3pkt 
-- Wykorzystano struktury dynamiczne (listy lub drzewa) 1pkt 
-- Poprawne zarządzanie pamięcią dynamiczną4Sprawdzanie czy udało się otworzyć, stworzyć plik 2pkt 
-- Program jest napisany optymalnie (jednokrotnie przegląda pliki) 2pkt 
-- Ogólne wrażenie 5pkt
-- Sprawozdanie 2pkt 
-- Występują zmienne globalne, pętle z warunkiem’true’-2pkt 
-- Występuje instrukcjagoto-2Wycieki pamięci -15pkt
-- Łączna liczba punktów : 30PKT
+[  ]  Program działa 2pkt
+[  ]  Zaimplementowano pełną funkcjonalność 3pkt
+[  ]  Kod programu jest podzielony na moduły 2pkt
+[  ]  Długość funkcji nie przekracza jednego ekranu 1pkt
+[  ]  Zadania są dobrze podzielone między funkcję 2pkt
+[  ]  Kod jest samokomentujący się lub występują komentarze 1pkt
+[  ]  Kod jest poprawnie sformatowany (wcięcia, jedna instrukcja w jednej linii, itp.)3pkt 
+[  ]  Wykorzystano struktury dynamiczne (listy lub drzewa) 1pkt 
+[  ]  Poprawne zarządzanie pamięcią dynamiczną4Sprawdzanie czy udało się otworzyć, stworzyć plik 2pkt 
+[  ]  Program jest napisany optymalnie (jednokrotnie przegląda pliki) 2pkt 
+[  ]  Ogólne wrażenie 5pkt
+[  ]  Sprawozdanie 2pkt 
+[  ]  Występują zmienne globalne, pętle z warunkiem’true’-2pkt 
+[  ]  Występuje instrukcjagoto-2Wycieki pamięci -15pkt
+[  ]  Łączna liczba punktów : 30PKT
 
-#Compilator: GCC
+# TO DO 
+
+[   ] Choose file arg
+[ X ] Open file
+[ - ] Create list
+[   ] Random quastion from list
+[   ] Creat files to other function
+
+# Compilator: GCC
+# Author: Jakub Przepióra
--- main.c
+++ main.c
@@ -3,48 +3,107 @@
 #include <stdio.h>
 #include <string.h>
 
-// struct DaneQuiz {
-//     char titleQuestion[50];
-//     int  goodAnswer;
-//     char firstAnswer[50];
-//     char secondAnswer[50];
-//     char thirdAnswer[50];
-//     char fourthAnswer[50];
-//     int  point;
-//     struct DaneQuiz * next;
-// } DaneQuiz;
+struct DaneQuiz {
+    char titleQuestion[50];
+    int  goodAnswer;
+    char firstAnswer[50];
+    char secondAnswer[50];
+    char thirdAnswer[50];
+    char fourthAnswer[50];
+    int  point;
+    struct DaneQuiz * next;
+} DaneQuiz;
+
+void createListQuestion() {
+    struct DaneQuiz daneDoQuizu;
+
+    FILE *plik = fopen("pytania.txt", "r");
+
+    // GET QUESTION
+    int znak = 0;
+    int i = 0;
+    while (znak != '\n')
+    {
+
+        znak = getc(plik);
+        daneDoQuizu.titleQuestion[i] = znak;
+        printf("%c", znak);
+        i++;
+    }
+
+    // GOOD ANSWER GET
+
+    znak = 0;
+
+    printf("Good Answer: ");
+    znak = getc(plik);
+    // if(!znak > 47 && !znak < 53) return;
+    daneDoQuizu.goodAnswer = znak - 48;
+    printf("%i ", daneDoQuizu.goodAnswer);
+
+    // ALL ANSWER GET
+    znak = getc(plik); //Clear end line
+    printf("%c", znak);
+    //// First ////
+
+    printf("1) ");
+    int j = 0;
+    znak = 0;
+    while (znak != '\n')
+    {
+
+        znak = getc(plik);
+        daneDoQuizu.firstAnswer[j] = znak;
+        printf("%c", znak);
+        j++;
+    }
+
+    //// Second ////
+    printf("2) ");
+    j = 0;
+    znak = 0;
+    while (znak != '\n')
+    {
+
+        znak = getc(plik);
+        daneDoQuizu.secondAnswer[j] = znak;
+        printf("%c", znak);
+        j++;
+    }
+
+    //// Third ////
+    printf("3) ");
+    j = 0;
+    znak = 0;
+    while (znak != '\n')
+    {
+
+        znak = getc(plik);
+        daneDoQuizu.thirdAnswer[j] = znak;
+        printf("%c", znak);
+        j++;
+    }
+    //// Fourth ////
+    printf("4) ");
+    j = 0;
+    znak = 0;
+    while (znak != '\n')
+    {
+
+        znak = getc(plik);
+        daneDoQuizu.fourthAnswer[j] = znak;
+        printf("%c", znak);
+        j++;
+    }
+    fclose(plik);
+}
 
 
 void showQuiz() {
-    char question[100];
-    char goodAns[2];
-    FILE *plik = fopen("pytania.txt", "r");
-
-    int znak = 0;
-    int i = 0;
-
-    do
-    {
-        printf("%c", znak);
-        znak = getc(plik);
-        question[i] = znak;
-        i++;
-    } while (znak != '\n');
-    // printf("\n\n%s", question);
-    int j = 0;
-    do{
-        printf("%d", znak);
-        znak = getc(plik);
-        goodAns[j] = znak;
-        j++;
-    } while (znak != '\n');
-
-    fclose(plik);
-
+    createListQuestion(); // Create list
 }
 
 int main() {
-    // struct DaneQuiz DaneDoQuizu;
     showQuiz();
 
     return 0;
