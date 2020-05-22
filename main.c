2a9aff99571c1a73a02651d33e703e402033959b
jakub-przepiora <jakub.przepioraa@gmail.com>
2020-05-22 23:32:31 +0200

    Inicialization args

--- main.c
+++ main.c
@@ -14,9 +14,8 @@ void saveStats(int allPoints, char user[32]) {
     return;
 }
 
-void checkAnsw(int *ans, int *goodAns) {
+void checkAnsw(char *nickname,int *ans, int *goodAns) {
     int points = 0;
-    char nickname[32];
     for (int i = 0; i < 10; i++) {
         if (goodAns[i] == ans[i]){
             points++;
@@ -24,37 +23,34 @@ void checkAnsw(int *ans, int *goodAns) {
         }
     }
     printf("goodans: %d \n", points);
-    printf("\nYour nickname: ");
-    scanf("%s\n" ,nickname);
+    
     saveStats(points, nickname);
     
     
 }
-void createListQuestion(){
+void createListQuestion(char *filename, char *name, int question){
     int znak = 0, i = 0, j = 0, odpowiedzi[10], goodAnsw[10], p = 0;           // INTIGER HELP VAARIABLE
     char titleQ[50], firstAns[50], secondAns[50], thirdAns[50], fourthAns[50]; // TMP VARIABLE 
-    FILE *plik = fopen("pytania.txt", "r"); // File open
+    FILE *plik = fopen(filename, "r"); // File open
     while (fscanf(plik, "%d" ,&znak) != EOF) {     // go to END OF FILE
         znak = 0; //clear cache start read question
         i = 0;    //clear cache start read question
-        printf("\n");
+        printf("\n============================================================\n");
         while (znak != 10){ // GET QUESTION
             znak = getc(plik);
             titleQ[i] = znak;
             printf("%c", znak);
             i++;
         }
-        // GOOD ANSWER GET 
         znak = 0;
         printf("Good Answer: "); // user for test
         znak = getc(plik);
-        goodAnsw[p] = znak - 48;
+        goodAnsw[p] = znak - 48;    // GOOD ANSWER GET
         printf("%i ", goodAnsw[p]); // user for test
         // ALL ANSWER GET
         znak = getc(plik); //Clear end line
         printf("%c", znak);
-        //// First ////
-        printf("1) ");
+        printf("1) "); //// First ////
         znak = 0;
         while (znak != '\n'){
             znak = getc(plik);
@@ -62,8 +58,7 @@ void createListQuestion(){
             printf("%c", znak);
             j++;
         }
-        //// Second ////
-        printf("2) ");
+        printf("2) "); //// Second ////
         j = 0;
         znak = 0;
         while (znak != '\n'){
@@ -72,8 +67,7 @@ void createListQuestion(){
             printf("%c", znak);
             j++;
         }
-        //// Third ////
-        printf("3) ");
+        printf("3) "); //// Third ////
         j = 0;
         znak = 0;
         while (znak != '\n'){
@@ -82,8 +76,7 @@ void createListQuestion(){
             printf("%c", znak);
             j++;
         }
-        //// Fourth ////
-        printf("4) ");
+        printf("4) "); //// Fourth ////
         j = 0;
         znak = 0;
         while (znak != '\n'){
@@ -101,16 +94,14 @@ void createListQuestion(){
                 odpowiedzi[p] = odpTMP;
                 p++;
                 wh = 1;
-            }
-            else {
+            } else {
                 printf("Error answear\n Choose other answear:");
                 scanf("%i", &odpTMP);
             }
         }
-        printf("\n=======================\n");
     }
     fclose(plik);
-    checkAnsw(goodAnsw, odpowiedzi);
+    checkAnsw(name,goodAnsw, odpowiedzi);
 }
 
 int main(int argc, char **argv) {
@@ -118,7 +109,7 @@ int main(int argc, char **argv) {
     char base[20], nick[32];
     char howMany[5];
     int que;
-    for(int i = 1; i < 7; i++){
+    for (int i = 1; i < argc; i++){
         // printf("%s\n", argv[i]);
         if (strcmp(argv[i], "-o") == 0){
             printf("ok\n");
@@ -130,19 +121,16 @@ int main(int argc, char **argv) {
         }
         else if (strcmp(argv[i], "-q") == 0){
             printf("ok\n");
-            // long conv = strtol(, &tmp, 10);
             strcpy(howMany, argv[i+1]);
             que = atoi(howMany);
         }
-
-        
     }
-    
-    printf("%s\n", base);
-    printf("%s\n", nick);
-    printf("%i\n", que);
 
-    createListQuestion();
+    // printf("%s\n", base);
+    // printf("%s\n", nick);
+    // printf("%i\n", que);
+
+    createListQuestion(base, nick, que);
 
     return 0;
 }
