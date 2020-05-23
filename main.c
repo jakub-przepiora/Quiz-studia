57b49c435b64075ed1ab9ef7b6fb9ca1daee57d6
jakub-przepiora <jakub.przepioraa@gmail.com>
2020-05-23 14:32:29 +0200

    all project

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 void saveStats(int allPoints, char user[32]) {
+    time_t rawtime;
+    struct tm *timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
     FILE * statsFile = fopen("stats.txt", "a");
     if (statsFile== NULL) {
         perror("Error opening file.");
     }
     else{
-        fprintf(statsFile, "%s: %d\n", user, allPoints);
+        fprintf(statsFile, "%s: %d | %s\n", user, allPoints, asctime(timeinfo));
         fclose(statsFile);
     }
     return;
@@ -19,7 +24,6 @@ void checkAnsw(char *nickname,int *ans, int *goodAns) {
     for (int i = 0; i < 10; i++) {
         if (goodAns[i] == ans[i]){
             points++;
-            
         }
     }
     printf("goodans: %d \n", points);
@@ -29,68 +33,68 @@ void checkAnsw(char *nickname,int *ans, int *goodAns) {
     
 }
 void createListQuestion(char *filename, char *name, int question){
-    int znak = 0, i = 0, j = 0, odpowiedzi[10], goodAnsw[10], p = 0;           // INTIGER HELP VAARIABLE
-    char titleQ[50], firstAns[50], secondAns[50], thirdAns[50], fourthAns[50]; // TMP VARIABLE 
-    FILE *plik = fopen(filename, "r"); // File open
-    // while (fscanf(plik, "%d" ,&znak) != EOF) {     // go to END OF FILE
-    for(int k = 0; k < question; k++){
+    int znak = 0, i = 0, j = 0, odpowiedzi[10], goodAnsw[10], p = 0, random = 0, questionShowed = 0;           // INTIGER HELP VAARIABLE
+    char titleQ[50], firstAns[50], secondAns[50], thirdAns[50], fourthAns[50]; // TMP VARIABLE
+    FILE *plik = fopen(filename, "r");         // File open
+    while (questionShowed != question && fscanf(plik, "%d", &znak) != EOF){ // go to END OF FILE
+        // for(int k = 0; k < question; k++){
+        random = rand();
         znak = 0; //clear cache start read question
         i = 0;    //clear cache start read question
-        printf("\n============================================================\n");
         while (znak != 10){ // GET QUESTION
             znak = getc(plik);
             titleQ[i] = znak;
-            printf("%c", znak);
+            if(random%2 == 0) printf("%c", znak);
             i++;
         }
         znak = 0;
-        printf("Good Answer: "); // user for test
+        if(random%2 == 0) printf("Good Answer: "); // user for test
         znak = getc(plik);
-        goodAnsw[p] = znak - 48;    // GOOD ANSWER GET
-        printf("%i ", goodAnsw[p]); // user for test
+        if(random%2 == 0) goodAnsw[p] = znak - 48;    // GOOD ANSWER GET
+        if(random%2 == 0) printf("%i ", goodAnsw[p]); // user for test
         // ALL ANSWER GET
         znak = getc(plik); //Clear end line
-        printf("%c", znak);
-        printf("1) "); //// First ////
+        if(random%2 == 0) printf("%c", znak);
+        if(random%2 == 0) printf("1) "); //// First ////
         znak = 0;
         while (znak != '\n'){
             znak = getc(plik);
             firstAns[j] = znak;
-            printf("%c", znak);
+            if(random%2 == 0) printf("%c", znak);
             j++;
         }
-        printf("2) "); //// Second ////
+        if(random%2 == 0) printf("2) "); //// Second ////
         j = 0;
         znak = 0;
         while (znak != '\n'){
             znak = getc(plik);
             secondAns[j] = znak;
-            printf("%c", znak);
+            if(random%2 == 0)printf("%c", znak);
             j++;
         }
-        printf("3) "); //// Third ////
+        if(random%2 == 0) printf("3) "); //// Third ////
         j = 0;
         znak = 0;
         while (znak != '\n'){
             znak = getc(plik);
             thirdAns[j] = znak;
-            printf("%c", znak);
+            if(random%2 == 0) printf("%c", znak);
             j++;
         }
-        printf("4) "); //// Fourth ////
+        if(random%2 == 0) printf("4) "); //// Fourth ////
         j = 0;
         znak = 0;
         while (znak != '\n'){
             znak = getc(plik);
             fourthAns[j] = znak;
-            printf("%c", znak);
+            if(random%2 == 0) printf("%c", znak);
             j++;
         }
-        printf("Podaj Twoja odpowiedz: ");
+        if(random%2 == 0) printf("Podaj Twoja odpowiedz: ");
         int odpTMP;
         int wh = 0;
-        scanf("%i",&odpTMP);
-        while ( wh != 1){
+        if(random%2 == 0) scanf("%i",&odpTMP);
+        if(random%2 == 0) while ( wh != 1){
             if(odpTMP > 0 && odpTMP < 5) {
                 odpowiedzi[p] = odpTMP;
                 p++;
@@ -100,6 +104,7 @@ void createListQuestion(char *filename, char *name, int question){
                 scanf("%i", &odpTMP);
             }
         }
+        if(random%2 == 0) questionShowed++;
     }
     fclose(plik);
     checkAnsw(name,goodAnsw, odpowiedzi);
